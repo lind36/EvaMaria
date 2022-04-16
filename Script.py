ddab99fbd4c506f5ab1eca610f9482c8b08f3bbd
Anonymous <102202494+ligyjoseph26@users.noreply.github.com>
2022-04-16 19:36:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a closed source project. 
-- Contact - https://t.me/+XrM0VNmBGz85MmU1 
+- Contact - https://t.me/ptstark 
 
 <b>DEVELOPER:</b>
 - <a href=https://t.me/+XrM0VNmBGz85MmU1>Team eva maria</a>"""
